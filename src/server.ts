f94fc3149dfa65c76198413170e3ce75741da9dc
Jhonny Lima <lima.jhonny95@gmail.com>
2024-06-24 21:01:14 -0300

    feat: add transactions routes file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
